d4b51057e7bb8d0e9bc881bed5a11a02735361e1
Mateusz Tymek <mtymek@gmail.com>
2011-11-20 23:17:43 +0100

    initial Application module

---
+++ configs/application.config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    'module_paths' => array(
+        realpath(__DIR__ . '/../modules'),
+//        realpath(__DIR__ . '/../vendors'),
+    ),
+    'modules' => array(
+        'Application',
+        'EdpMarkdown'
+    ),
+    'module_listener_options' => array( 
+        'config_cache_enabled'     => false,
+        'cache_dir'                => realpath(__DIR__ . '/../data/cache'),
+    ),
+);
